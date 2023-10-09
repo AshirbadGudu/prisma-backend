567a05320e7d7996131463e92477b3b24f14bfec
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-10-09 15:09:22 +0530

    config file created

--- server/configs/index.ts
+++ server/configs/index.ts
@@ -1,1 +1,15 @@
-export {};
+import { config } from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+config();
+
+const configs = {
+  PORT: process.env.PORT,
+  JWT_SECRET: `${process.env.JWT_SECRET}`,
+  API_VERSION: `api/v1`,
+  HOST: `${process.env.HOST}`,
+};
+
+export { configs, prisma };
