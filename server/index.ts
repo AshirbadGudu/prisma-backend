d37730b2a3f8d3c99aaf6d785133559f9ed774c2
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-10-09 16:08:56 +0530

    server running

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,15 @@
-export {};
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import { ListenerPlugin, RouterPlugin } from "./plugins";
+
+const app = express();
+
+app
+  .use(helmet())
+  .use(cors())
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }));
+
+RouterPlugin.setup(app);
+ListenerPlugin.listen(app);
