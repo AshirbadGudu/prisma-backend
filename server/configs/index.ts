fc35426af603ff37698584bd0245d3f3690d8894
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-10-10 12:17:42 +0530

    firebase admin setup complete

--- server/configs/index.ts
+++ server/configs/index.ts
@@ -10,13 +10,19 @@ const configs = {
   JWT_SECRET: `${process.env.JWT_SECRET}`,
   API_VERSION: `api/v1`,
   HOST: `${process.env.HOST}`,
+  // Mail Configs
   MAIL_SERVICE: `${process.env.MAIL_SERVICE}`,
   MAIL_PORT: process.env.MAIL_PORT,
   MAIL_HOST: `${process.env.MAIL_HOST}`,
   MAIL_FROM: `${process.env.MAIL_FROM}`,
   MAIL_USER: `${process.env.MAIL_USER}`,
   MAIL_PASS: `${process.env.MAIL_PASS}`,
+  // Firebase Configs
+  PROJECT_ID: `${process.env.PROJECT_ID}`,
+  PRIVATE_KEY: `${process.env.PRIVATE_KEY}`,
+  CLIENT_EMAIL: `${process.env.CLIENT_EMAIL}`,
 };
 
 export * from "./mail.config";
+export * from "./firebase.config";
 export { configs, prisma };
