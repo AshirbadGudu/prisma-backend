4c875c5e538d85b7d2413621eb58aefe46bacf66
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-10-11 12:35:43 +0530

    conversion of file logic implemented with sharp

--- server/controllers/file.controller.ts
+++ server/controllers/file.controller.ts
@@ -1,9 +1,11 @@
 import { RequestHandler } from "express";
 import fs from "fs";
+import sharp from "sharp";
 
 export const fileController: {
   upload: RequestHandler;
   delete: RequestHandler;
+  uploadWebp: RequestHandler;
 } = {
   upload(req, res, next) {
     try {
@@ -51,6 +53,45 @@ export const fileController: {
         res.status(404).json({
           success: false,
           msg: "File not found",
+        });
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async uploadWebp(req, res, next) {
+    try {
+      // Explicitly specify the type for req.files
+      const files = req.files as Express.Multer.File[];
+      if (files) {
+        const webpImages = await Promise.all(
+          files.map(async (file) => {
+            return sharp(file.path)
+              .webp()
+              .toBuffer()
+              .then((webpBuffer) => {
+                const webpFileName = "converted_" + file.originalname + ".webp";
+                // Save the WebP file to a folder
+                const outputPath = "./uploads/webp/" + webpFileName;
+                fs.writeFileSync(outputPath, webpBuffer);
+                return {
+                  originalname: file.originalname,
+                  webpFileName: webpFileName,
+                };
+              });
+          })
+        );
+
+        res.json({
+          success: true,
+          msg: "Files uploaded and converted to WebP successfully",
+          files: webpImages,
+        });
+      } else {
+        res.status(400).json({
+          success: false,
+          msg: "No files uploaded",
         });
       }
     } catch (error) {
--- server/routes/file.routes.ts
+++ server/routes/file.routes.ts
@@ -4,6 +4,7 @@ import { fileController } from "../controllers";
 const router = Router();
 
 router.post("/", fileController.upload);
+router.post("/webp", fileController.uploadWebp);
 router.delete("/", fileController.delete);
 
 export default router;
